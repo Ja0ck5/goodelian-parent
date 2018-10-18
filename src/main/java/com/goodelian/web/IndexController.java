5b3c33b1949c2f5b1fea9e7d870a2e5d818303b2
ja0ck5 <975117619@qq.com>
2018-10-18 09:28:50 +0800

    mail

--- src/main/java/com/goodelian/web/IndexController.java
+++ src/main/java/com/goodelian/web/IndexController.java
@@ -1,6 +1,9 @@
 package com.goodelian.web;
 
 import com.goodelian.model.Concat;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +13,9 @@ public class IndexController {
 
 	private static final String ERROR_PATH = "/index";
 
+	@Autowired
+	private JavaMailSender javaMailSender;
+
 	@GetMapping("/")
 	public String index(ModelMap map) {
 		// return模板文件的名称，对应src/main/resources/templates/index.html
@@ -17,17 +23,29 @@ public class IndexController {
 		return "index";
 	}
 
+	protected static final String MY_MAIL = "15112222289@163.com";
+
 	@PostMapping("/concat")
 	@ResponseBody
-	public String concat(@ModelAttribute Concat concat) {
-		return null;
+	public void concat(@ModelAttribute Concat concat) {
+		if (concat == null || concat.getMessage().trim().length() == 0
+				|| concat.getMessage().trim().length() > 102400) {
+			return;
+		}
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setFrom(MY_MAIL);
+		message.setTo(MY_MAIL);
+		message.setSubject(concat.getSubject());
+		message.setText(concat.getMessage());
+		javaMailSender.send(message);
+		// TODO save from who
 	}
 
 	@GetMapping("/test")
 	public String test(ModelMap map) {
 		// return模板文件的名称，对应src/main/resources/templates/index.html
 		map.addAttribute("concat", new Concat());
-		int a = 1/0;
+		int a = 1 / 0;
 		return "index";
 	}
 }
--- src/test/java/com/goodelian/GoodelianParentApplicationTests.java
+++ src/test/java/com/goodelian/GoodelianParentApplicationTests.java
@@ -2,7 +2,10 @@ package com.goodelian;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -11,6 +14,20 @@ public class GoodelianParentApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Autowired
+	private JavaMailSender mailSender;
+
+	@Test
+	public void sendSimpleMail() throws Exception {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setFrom("ja0ck5@163.com");
+		message.setTo("ja0ck5@163.com");
+		message.setSubject("主题：简单邮件");
+		message.setText("测试邮件内容");
+
+		mailSender.send(message);
 	}
 
 }
